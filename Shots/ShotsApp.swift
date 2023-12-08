2071872181c9c4c6af75256a157c928b7e0da431
Brett Rosen <Brejuro23@gmail.com>
2023-12-08 17:42:01 -0500

    Initial Commit

---
+++ Shots/ShotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShotsApp.swift
+//  Shots
+//
+//  Created by Brett Rosen on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ShotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
